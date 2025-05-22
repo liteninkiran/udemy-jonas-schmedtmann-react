abd707624d1b7604e743206a43dc56e9253acb93
Kiran Anand <liteninkiran@hotmail.com>
2025-05-22 17:12:33 +0100

    053 - Setting Classes and Text Conditionally

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -14,18 +14,15 @@ function App() {
 }
 
 function Pizza({ pizza }) {
-    if (pizza.soldOut) {
-        return null;
-    }
     return (
-        <div className='pizza'>
+        <li className={`pizza ${pizza.soldOut ? 'sold-out' : ''}`}>
             <img src={pizza.photoName} alt={pizza.name} />
             <div>
                 <h3>{pizza.name}</h3>
                 <p>{pizza.ingredients}</p>
-                <span>£{pizza.price}</span>
+                <span>{pizza.soldOut ? 'SOLD OUT' : pizza.price}</span>
             </div>
-        </div>
+        </li>
     );
 }
 
