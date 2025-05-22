bde52a4bc6050357d26e30ada939e45ff35f658a
Kiran Anand <liteninkiran@hotmail.com>
2025-05-22 13:15:02 +0100

    036 - Creating And Reusing a Component

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { pizzaData } from './data';
 import './index.css';
 
 function App() {
+    console.log(pizzaData);
     return (
         <div className='container'>
-            <h1>Hello</h1>
+            <Pizza />
+        </div>
+    );
+}
+
+function Pizza() {
+    return (
+        <div>
+            <img src='pizzas/spinaci.jpg' alt='Pizza Spinaci' />
+            <h2>Pizza Spinaci</h2>
+            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
         </div>
     );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    // <React.StrictMode>
+    <App />
+    // </React.StrictMode>
 );
