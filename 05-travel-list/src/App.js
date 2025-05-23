793d9ecdc59b708d9d7c9aa72b9f2b4929b87dcb
Kiran Anand <liteninkiran@hotmail.com>
2025-05-23 13:38:41 +0100

    073 - Controlled Elements

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialItems = [
     { id: 1, description: 'Passports', quantity: 2, packed: false },
     { id: 2, description: 'Socks', quantity: 12, packed: true },
@@ -20,21 +22,44 @@ function Logo() {
 }
 
 function Form() {
+    const [description, setDescription] = useState('');
+    const [quantity, setQuantity] = useState(1);
     const options = Array.from({ length: 20 }, (_, i) => i + 1);
     const mapFn = (num) => (
         <option value={num} key={num}>
             {num}
         </option>
     );
+    const resetForm = () => {
+        setDescription('');
+        setQuantity(1);
+    };
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(e);
+        if (!description) return;
+        const newItem = {
+            description,
+            quantity,
+            packed: false,
+            id: Date.now(),
+        };
+        console.log(newItem);
+        resetForm();
     };
+    const handleOnDescriptionChange = (e) => setDescription(e.target.value);
+    const handleOnQuantityChange = (e) => setQuantity(Number(e.target.value));
     return (
         <form className='add-form' onSubmit={handleSubmit}>
             <h3>What do you need for your 😍 trip?</h3>
-            <select>{options.map(mapFn)}</select>
-            <input type='text' placeholder='Item...' />
+            <select value={quantity} onChange={handleOnQuantityChange}>
+                {options.map(mapFn)}
+            </select>
+            <input
+                type='text'
+                placeholder='Item...'
+                value={description}
+                onChange={handleOnDescriptionChange}
+            />
             <button>Add</button>
         </form>
     );
