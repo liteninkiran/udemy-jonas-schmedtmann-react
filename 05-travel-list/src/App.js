d179e96a8c6c5a283d30293381dc24185c7d0efb
Kiran Anand <liteninkiran@hotmail.com>
2025-05-23 18:13:11 +0100

    086 - Sorting Items

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -73,7 +73,9 @@ function Form({ onAddItems }) {
     );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
+    const [sortBy, setSortBy] = useState('input');
+    const onChange = (e) => setSortBy(e.target.value);
     const mapFn = (item) => (
         <Item
             item={item}
@@ -82,9 +84,29 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
             onToggleItem={onToggleItem}
         />
     );
+
+    let sortedItems = [];
+
+    const sortByDescription = (a, b) =>
+        a.description.localeCompare(b.description);
+    const sortByPacked = (a, b) => Number(a.packed) - Number(b.packed);
+
+    if (sortBy === 'input') sortedItems = items;
+    if (sortBy === 'descr') sortedItems = items.slice().sort(sortByDescription);
+    if (sortBy === 'packd') sortedItems = items.slice().sort(sortByPacked);
+
     return (
         <div className='list'>
-            <ul>{items.map(mapFn)}</ul>
+            <ul>{sortedItems.map(mapFn)}</ul>
+
+            <div className='actions'>
+                <select value={sortBy} onChange={onChange}>
+                    <option value='input'>Sort by input order</option>
+                    <option value='descr'>Sort by description</option>
+                    <option value='packd'>Sort by packed status</option>
+                </select>
+                <button onClick={onClearList}>Clear list</button>
+            </div>
         </div>
     );
 }
