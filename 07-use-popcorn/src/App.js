ae5dbabd398122ac84e12fa610be90da78acb4de
Kiran Anand <liteninkiran@hotmail.com>
2025-06-20 16:01:01 +0100

    147 - Handling Errors

--- 07-use-popcorn/src/App.js
+++ 07-use-popcorn/src/App.js
@@ -6,6 +6,7 @@ import MovieList from './components/MovieList';
 import WatchedMovies from './components/WatchedMovies';
 import Loader from './components/Loader';
 import ListBox from './components/ListBox';
+import ErrorMessage from './components/ErrorMessage';
 
 const key = '5b8881e2';
 const baseUrl = 'http://www.omdbapi.com/';
@@ -13,16 +14,35 @@ const baseUrl = 'http://www.omdbapi.com/';
 export default function App() {
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const query = 'interstellar';
+    const [error, setError] = useState('');
+    // const query = 'interstellar';
+    const query = 'sdafdasd';
     const url = `${baseUrl}?apikey=${key}&s=${query}`;
 
     const fetchData = () => {
         async function fetchMovies() {
-            setIsLoading(true);
-            const res = await fetch(url);
-            const data = await res.json();
-            setMovies(data.Search);
-            setIsLoading(false);
+            try {
+                setIsLoading(true);
+                const res = await fetch(url);
+
+                if (!res.ok) {
+                    throw new Error(
+                        'Something went wrong with fetching movies'
+                    );
+                }
+
+                const data = await res.json();
+
+                if (data.Response === 'False') {
+                    throw new Error('No movies match this search criteria');
+                }
+
+                setMovies(data.Search);
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setIsLoading(false);
+            }
         }
         fetchMovies();
     };
@@ -36,7 +56,13 @@ export default function App() {
             </NavBar>
             <Main>
                 <ListBox>
-                    {isLoading ? <Loader /> : <MovieList movies={movies} />}
+                    {isLoading ? (
+                        <Loader />
+                    ) : error ? (
+                        <ErrorMessage message={error} />
+                    ) : (
+                        <MovieList movies={movies} />
+                    )}
                 </ListBox>
                 <WatchedMovies />
             </Main>
