9bfd9621ec48a3257a55eb82f3bb15292ae60655
Kiran Anand <liteninkiran@hotmail.com>
2025-06-20 15:47:29 +0100

    146 - Adding a Loading State

--- 07-use-popcorn/src/App.js
+++ 07-use-popcorn/src/App.js
@@ -4,21 +4,25 @@ import Main from './components/Main';
 import Search from './components/Search';
 import MovieList from './components/MovieList';
 import WatchedMovies from './components/WatchedMovies';
+import Loader from './components/Loader';
+import ListBox from './components/ListBox';
 
 const key = '5b8881e2';
 const baseUrl = 'http://www.omdbapi.com/';
 
 export default function App() {
     const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     const query = 'interstellar';
     const url = `${baseUrl}?apikey=${key}&s=${query}`;
 
     const fetchData = () => {
         async function fetchMovies() {
+            setIsLoading(true);
             const res = await fetch(url);
             const data = await res.json();
             setMovies(data.Search);
-            console.log(data.Search);
+            setIsLoading(false);
         }
         fetchMovies();
     };
@@ -31,7 +35,9 @@ export default function App() {
                 <Search movies={movies} />
             </NavBar>
             <Main>
-                <MovieList movies={movies} />
+                <ListBox>
+                    {isLoading ? <Loader /> : <MovieList movies={movies} />}
+                </ListBox>
                 <WatchedMovies />
             </Main>
         </>
--- 07-use-popcorn/src/components/MovieList.js
+++ 07-use-popcorn/src/components/MovieList.js
@@ -1,15 +1,10 @@
 import ListItem from './ListItem';
-import ListBox from './ListBox';
 
 const MovieList = ({ movies }) => {
     const mapFn = (movie) => (
         <ListItem movie={movie} type={2} key={movie.imdbID} />
     );
-    return (
-        <ListBox>
-            <ul className='list'>{movies?.map(mapFn)}</ul>
-        </ListBox>
-    );
+    return <ul className='list'>{movies?.map(mapFn)}</ul>;
 };
 
 export default MovieList;
