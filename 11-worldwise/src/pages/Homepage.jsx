b3826124bfaf5b77b545038a121c133905b9ddfa
Kiran Anand <liteninkiran@hotmail.com>
2025-06-23 18:16:45 +0100

    209 - Using CSS Modules

--- 11-worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import PageNav from '../components/PageNav';
+import AppNav from '../components/AppNav';
 
 const Homepage = () => {
     return (
         <div>
             <PageNav />
+            <AppNav />
             <h1>Worldwise</h1>
-            <Link to='/pricing'>Pricing</Link>
+            <Link to='/app'>App Layout</Link>
         </div>
     );
 };
