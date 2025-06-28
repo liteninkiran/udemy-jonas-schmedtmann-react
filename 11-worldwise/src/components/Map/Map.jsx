9360529af03b19fd600cf0652afae147a0848b00
Kiran Anand <liteninkiran@hotmail.com>
2025-06-28 16:16:30 +0100

    231 - Displaying City Markers on Map

--- 11-worldwise/src/components/Map/Map.jsx
+++ 11-worldwise/src/components/Map/Map.jsx
@@ -1,31 +1,33 @@
 import { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { useCities } from '../../contexts/Cities/useCities';
 import styles from './Map.module.css';
+
+const url = 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
 
 const Map = () => {
     const nav = useNavigate();
+    const { cities } = useCities();
     const [searchParams, setSearchParams] = useSearchParams();
-    const lat = searchParams.get('lat');
-    const lng = searchParams.get('lng');
     const [position, setPosition] = useState([40, 0]);
+    const getPosition = (city) => [city.position.lat, city.position.lng];
+    const mapCities = (city) => (
+        <Marker position={getPosition(city)} key={city.id}>
+            <Popup>{city.cityName}</Popup>
+        </Marker>
+    );
+    const containerProps = {
+        center: position,
+        zoom: 13,
+        scrollWheelZoom: true,
+        className: styles.mapContainer,
+    };
     return (
         <div className={styles.mapContainer}>
-            <MapContainer
-                center={position}
-                zoom={13}
-                scrollWheelZoom={true}
-                className={styles.mapContainer}
-            >
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
-                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                />
-                <Marker position={position}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+            <MapContainer {...containerProps}>
+                <TileLayer url={url} />
+                {cities.map(mapCities)}
             </MapContainer>
         </div>
     );
