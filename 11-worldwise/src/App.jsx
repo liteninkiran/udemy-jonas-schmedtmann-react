ebc5d3e25b84e2b149a96e46f7f4487771ae14e6
Kiran Anand <liteninkiran@hotmail.com>
2025-06-28 12:54:34 +0100

    227 - Back to "WorldWise": Creating a CitiesContext

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import Homepage from './pages/Homepage/Homepage';
@@ -12,67 +11,36 @@ import CityList from './components/CityList/CityList';
 import CountryList from './components/CountryList/CountryList';
 import City from './components/City/City';
 import Form from './components/Form/Form';
+import { CitiesProvider } from './contexts/CitiesContext';
 
-const BASE_URL = 'http://localhost:9001';
+const future = {
+    v7_startTransition: true,
+    v7_relativeSplatPath: true,
+};
 
 const App = () => {
-    const [cities, setCities] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-
-    const fetchCityData = () => {
-        const fetchCities = async () => {
-            try {
-                setIsLoading(true);
-                const url = `${BASE_URL}/cities`;
-                const res = await fetch(url);
-                const data = await res.json();
-                setCities(data);
-            } catch (err) {
-                alert('Error fetching data');
-            } finally {
-                setIsLoading(false);
-            }
-        };
-        fetchCities();
-    };
-
-    useEffect(fetchCityData, []);
-
     return (
-        <BrowserRouter
-            future={{
-                v7_startTransition: true,
-                v7_relativeSplatPath: true,
-            }}
-        >
-            <Routes>
-                <Route index element={<Homepage />} />
-                <Route path='product' element={<Product />} />
-                <Route path='pricing' element={<Pricing />} />
-                <Route path='login' element={<Login />} />
-                <Route path='app' element={<AppLayout />}>
-                    <Route index element={<Navigate to='cities' replace />} />
-                    <Route
-                        path='cities'
-                        element={
-                            <CityList cities={cities} isLoading={isLoading} />
-                        }
-                    />
-                    <Route path='cities/:id' element={<City />}></Route>
-                    <Route
-                        path='countries'
-                        element={
-                            <CountryList
-                                cities={cities}
-                                isLoading={isLoading}
-                            />
-                        }
-                    />
-                    <Route path='form' element={<Form />} />
-                </Route>
-                <Route path='*' element={<PageNotFound />} />
-            </Routes>
-        </BrowserRouter>
+        <CitiesProvider>
+            <BrowserRouter future={future}>
+                <Routes>
+                    <Route index element={<Homepage />} />
+                    <Route path='product' element={<Product />} />
+                    <Route path='pricing' element={<Pricing />} />
+                    <Route path='login' element={<Login />} />
+                    <Route path='app' element={<AppLayout />}>
+                        <Route
+                            index
+                            element={<Navigate to='cities' replace />}
+                        />
+                        <Route path='cities' element={<CityList />} />
+                        <Route path='cities/:id' element={<City />}></Route>
+                        <Route path='countries' element={<CountryList />} />
+                        <Route path='form' element={<Form />} />
+                    </Route>
+                    <Route path='*' element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </CitiesProvider>
     );
 };
 
---
+++ 11-worldwise/src/contexts/CitiesContext.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { createContext } from 'react';
+
+const BASE_URL = 'http://localhost:9001';
+
+const CitiesContext = createContext();
+
+const CitiesProvider = ({ children }) => {
+    const [cities, setCities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchCityData = () => {
+        const fetchCities = async () => {
+            try {
+                setIsLoading(true);
+                const url = `${BASE_URL}/cities`;
+                const res = await fetch(url);
+                const data = await res.json();
+                setCities(data);
+            } catch (err) {
+                alert('Error fetching data');
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchCities();
+    };
+
+    useEffect(fetchCityData, []);
+
+    const value = { cities, isLoading };
+
+    return (
+        <CitiesContext.Provider value={value}>
+            {children}
+        </CitiesContext.Provider>
+    );
+};
+
+export { CitiesProvider };
