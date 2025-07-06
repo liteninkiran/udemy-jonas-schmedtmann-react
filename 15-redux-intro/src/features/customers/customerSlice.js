813692c60749cf30cbb742a87e4c9ab21ba8fb0e
Kiran Anand <liteninkiran@hotmail.com>
2025-07-06 19:59:40 +0100

    277 - Creating the Customer Slice

--- 15-redux-intro/src/features/customers/customerSlice.js
+++ 15-redux-intro/src/features/customers/customerSlice.js
@@ -1,38 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 const initialState = {
     fullName: '',
     nationalId: '',
     createdAt: '',
 };
 
-export const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'customer/createCustomer':
-            return {
-                ...state,
-                fullName: action.payload.fullName,
-                nationalId: action.payload.nationalId,
-                createdAt: action.payload.createdAt,
-            };
-        case 'customer/updateName':
-            return {
-                ...state,
-                fullName: action.payload,
-            };
-        default:
-            return state;
-    }
-};
-
-export const createCustomer = (fullName, nationalId) => ({
-    type: 'customer/createCustomer',
-    payload: {
-        fullName,
-        nationalId,
-        createdAt: new Date().toISOString(),
+const customerSlice = createSlice({
+    name: 'customer',
+    initialState,
+    reducers: {
+        createCustomer: {
+            prepare(fullName, nationalId) {
+                return {
+                    payload: {
+                        fullName,
+                        nationalId,
+                        createdAt: new Date().toISOString(),
+                    },
+                };
+            },
+            reducer(state, action) {
+                state.fullName = action.payload.fullName;
+                state.nationalId = action.payload.nationalId;
+                state.createdAt = action.payload.createdAt;
+            },
+        },
+        updateName(state, action) {
+            state.fullName = action.payload;
+        },
     },
 });
 
-export const updateName = (fullName) => ({
-    type: 'customer/updateName',
-    payload: fullName,
-});
+export const { createCustomer, updateName } = customerSlice.actions;
+export default customerSlice.reducer;
--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import accountReducer from './features/accounts/accountSlice';
-import { reducer as customerReducer } from './features/customers/customerSlice';
+import customerReducer from './features/customers/customerSlice';
 
 const store = configureStore({
     reducer: {
