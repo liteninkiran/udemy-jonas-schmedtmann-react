2b5b803d470194520759bd82d4e8a29f2d1d2180
Kiran Anand <liteninkiran@hotmail.com>
2025-07-06 15:48:30 +0100

    267 - Back to React! Connecting our Redux App With React

--- 15-redux-intro/src/index.js
+++ 15-redux-intro/src/index.js
@@ -2,19 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './store';
-import './store';
 import './index.css';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
-
-store.dispatch({
-    type: 'account/deposit',
-    payload: 250,
-});
-
-console.log(store.getState());
