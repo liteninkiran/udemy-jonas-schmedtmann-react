3eed4906c98ddb45e24b11e4f0c064efa189c3ee
Kiran Anand <liteninkiran@hotmail.com>
2025-07-07 17:20:13 +0100

    285 - A New Way Of Implementing Routes

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -1,5 +1,38 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />,
+    },
+    {
+        path: '/menu',
+        element: <Menu />,
+    },
+    {
+        path: '/cart',
+        element: <Cart />,
+    },
+    {
+        path: '/order/new',
+        element: <CreateOrder />,
+    },
+    {
+        path: '/order/:orderId',
+        element: <Order />,
+    },
+]);
+
 const App = () => {
-    return <h1>Hello</h1>;
+    return (
+        <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    );
 };
 
 export default App;
