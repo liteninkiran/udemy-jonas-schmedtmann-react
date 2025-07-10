b83497460e3cd08c6f20c7a2d14f5027c8178ee3
Kiran Anand <liteninkiran@hotmail.com>
2025-07-10 12:57:31 +0100

    297 - Styling Text

--- 16-fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/src/ui/Header.jsx
@@ -1,13 +1,15 @@
+import Username from '../features/user/Username';
 import SearchOrder from '../features/order/SearchOrder';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+        <header className='flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6'>
             <Link to='/' className='tracking-widest'>
                 Fast React Pizza Co.
             </Link>
             <SearchOrder />
+            <Username />
         </header>
     );
 };
