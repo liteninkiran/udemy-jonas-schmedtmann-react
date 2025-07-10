8960193ce208f0635d9a9fb86c08804684ecaa2b
Kiran Anand <liteninkiran@hotmail.com>
2025-07-10 16:48:48 +0100

    304 - Reusing Styles With @apply

--- 16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -2,114 +2,135 @@ import { useState } from "react";
 import { Form, useActionData, useNavigation } from "react-router-dom";
 
 const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
+    {
+        pizzaId: 12,
+        name: "Mediterranean",
+        quantity: 2,
+        unitPrice: 16,
+        totalPrice: 32,
+    },
+    {
+        pizzaId: 6,
+        name: "Vegetale",
+        quantity: 1,
+        unitPrice: 13,
+        totalPrice: 13,
+    },
+    {
+        pizzaId: 11,
+        name: "Spinach and Mushroom",
+        quantity: 1,
+        unitPrice: 15,
+        totalPrice: 15,
+    },
 ];
 
 const CreateOrder = () => {
-  // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
-  const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
-  const formErrors = useActionData();
-  const buttonClasses = [
-    "bg-yellow-400",
-    "uppercase",
-    "tracking-wide",
-    "font-semibold",
-    "text-stone-800",
-    "py-3",
-    "px-4",
-    "inline-block",
-    "rounded-full",
-    "hover:bg-yellow-300",
-    "transition-colors",
-    "duration-300",
-    "focus:outline-none",
-    "focus:ring",
-    "focus:ring-yellow-300",
-    "focus:bg-yellow-300",
-    "focus:ring-offset-2",
-    "disabled:cursor-not-allowed",
-  ];
+    // const [withPriority, setWithPriority] = useState(false);
+    const cart = fakeCart;
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === "submitting";
+    const formErrors = useActionData();
+    const buttonClasses = [
+        "bg-yellow-400",
+        "uppercase",
+        "tracking-wide",
+        "font-semibold",
+        "text-stone-800",
+        "py-3",
+        "px-4",
+        "inline-block",
+        "rounded-full",
+        "hover:bg-yellow-300",
+        "transition-colors",
+        "duration-300",
+        "focus:outline-none",
+        "focus:ring",
+        "focus:ring-yellow-300",
+        "focus:bg-yellow-300",
+        "focus:ring-offset-2",
+        "disabled:cursor-not-allowed",
+    ];
 
-  return (
-    <div>
-      <h2>Ready to order? Lets go!</h2>
-
-      <Form method="POST">
-        {/* First Name */}
+    return (
         <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required />
-        </div>
+            <h2>Ready to order? Lets go!</h2>
 
-        {/* Phone Number */}
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required />
-            {formErrors?.phone && <p>{formErrors.phone}</p>}
-          </div>
-        </div>
+            <Form method="POST">
+                {/* First Name */}
+                <div>
+                    <label>First Name</label>
+                    <input
+                        type="text"
+                        placeholder="Enter your name"
+                        name="customer"
+                        required
+                        className="input"
+                    />
+                </div>
 
-        {/* Address */}
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
-          <label className="sm:basis-40">Address</label>
-          <div className="grow">
-            <input
-              type="text"
-              name="address"
-              placeholder="Enter your address"
-              required
-              className="input w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
-            />
-          </div>
-        </div>
+                {/* Phone Number */}
+                <div>
+                    <label>Phone number</label>
+                    <div>
+                        <input
+                            type="tel"
+                            name="phone"
+                            placeholder="Enter your telephone number"
+                            required
+                            className="input"
+                        />
+                        {formErrors?.phone && <p>{formErrors.phone}</p>}
+                    </div>
+                </div>
 
-        {/* Priority */}
-        <div>
-          <input
-            type="checkbox"
-            name="priority"
-            id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
-            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
-          />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
-        </div>
+                {/* Address */}
+                <div>
+                    <label>Address</label>
+                    <div className="grow">
+                        <input
+                            type="text"
+                            name="address"
+                            placeholder="Enter your address"
+                            required
+                            className="input"
+                        />
+                    </div>
+                </div>
 
-        {/* Submit */}
-        <div>
-          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+                {/* Priority */}
+                <div>
+                    <input
+                        type="checkbox"
+                        name="priority"
+                        id="priority"
+                        // value={withPriority}
+                        // onChange={(e) => setWithPriority(e.target.checked)}
+                        className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
+                    />
+                    <label htmlFor="priority">
+                        Want to yo give your order priority?
+                    </label>
+                </div>
 
-          <button disabled={isSubmitting} className={buttonClasses.join(" ")}>
-            {isSubmitting ? "Placing order...." : `Order now`}
-          </button>
+                {/* Submit */}
+                <div>
+                    <input
+                        type="hidden"
+                        name="cart"
+                        value={JSON.stringify(cart)}
+                    />
+
+                    <button
+                        disabled={isSubmitting}
+                        className={buttonClasses.join(" ")}
+                    >
+                        {isSubmitting ? "Placing order...." : `Order now`}
+                    </button>
+                </div>
+            </Form>
         </div>
-      </Form>
-    </div>
-  );
+    );
 };
 
 export default CreateOrder;
