3e4509ac5892bbc87229700571512aa6f0be6305
Kiran Anand <liteninkiran@hotmail.com>
2025-07-10 12:51:33 +0100

    296 - Working With Color

--- 16-fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/src/ui/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header>
+        <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
             <Link to='/' className='tracking-widest'>
                 Fast React Pizza Co.
             </Link>
