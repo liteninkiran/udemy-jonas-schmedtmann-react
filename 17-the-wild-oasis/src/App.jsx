cd44a33bc153c1cf04ff00aaa2f47dab99a5e687
Kiran Anand <liteninkiran@hotmail.com>
2025-07-14 16:52:02 +0100

    334 - Building the App Layout

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
+
 import GlobalStyles from './styles/GlobalStyles';
+import Dashboard from './pages/Dashboard';
 import Bookings from './pages/Bookings';
 import Cabins from './pages/Cabins';
 import Users from './pages/Users';
@@ -8,23 +9,32 @@ import Settings from './pages/Settings';
 import Account from './pages/Account';
 import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
+import AppLayout from './ui/AppLayout';
+
+const future = {
+    v7_startTransition: true,
+    v7_relativeSplatPath: true,
+};
 
 const App = () => {
     return (
         <>
             <GlobalStyles />
-            <BrowserRouter>
+            <BrowserRouter future={future}>
                 <Routes>
-                    <Route
-                        index
-                        element={<Navigate replace to='dashboard' />}
-                    />
-                    <Route path='dashboard' element={<Dashboard />} />
-                    <Route path='bookings' element={<Bookings />} />
-                    <Route path='cabins' element={<Cabins />} />
-                    <Route path='users' element={<Users />} />
-                    <Route path='settings' element={<Settings />} />
-                    <Route path='account' element={<Account />} />
+                    <Route element={<AppLayout />}>
+                        <Route
+                            index
+                            element={<Navigate replace to='dashboard' />}
+                        />
+                        <Route path='dashboard' element={<Dashboard />} />
+                        <Route path='bookings' element={<Bookings />} />
+                        <Route path='cabins' element={<Cabins />} />
+                        <Route path='users' element={<Users />} />
+                        <Route path='settings' element={<Settings />} />
+                        <Route path='account' element={<Account />} />
+                    </Route>
+
                     <Route path='login' element={<Login />} />
                     <Route path='*' element={<PageNotFound />} />
                 </Routes>
