6726e268db47872d6694f2999e5c8e117697effb
Kiran Anand <liteninkiran@hotmail.com>
2025-07-17 15:39:45 +0100

    368 - Converting the Modal to a Compound Component

--- 17-the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ 17-the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,24 +1,19 @@
-import { useState } from 'react';
-
 import Button from '@ui/Button';
 import Modal from '@ui/Modal';
 import CreateCabinForm from './CreateCabinForm';
 
 const AddCabin = () => {
-    const [isOpenModal, setIsOpenModal] = useState(false);
     return (
-        <>
-            <Button onClick={() => setIsOpenModal((show) => !show)}>
-                Add New Cabin
-            </Button>
-            {isOpenModal && (
-                <Modal onClose={() => setIsOpenModal(false)}>
-                    <CreateCabinForm
-                        onCloseModal={() => setIsOpenModal(false)}
-                    />
-                </Modal>
-            )}
-        </>
+        <div>
+            <Modal>
+                <Modal.Open opens='cabin-form'>
+                    <Button>Add new cabin</Button>
+                </Modal.Open>
+                <Modal.Window name='cabin-form'>
+                    <CreateCabinForm />
+                </Modal.Window>
+            </Modal>
+        </div>
     );
 };
 
