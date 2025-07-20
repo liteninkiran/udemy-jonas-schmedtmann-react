1ceceb56ae9e2964d13a7cfeef9dbb464feb7981
Kiran Anand <liteninkiran@hotmail.com>
2025-07-20 22:27:29 +0100

    416 - Experiment: Manual SSR With React DOM + Node.js

---
+++ 20-manual-ssr/data.js
@@ -0,0 +1,22 @@
+export const pizzas = [
+    {
+        name: 'Focaccia',
+        price: 6,
+    },
+    {
+        name: 'Pizza Margherita',
+        price: 10,
+    },
+    {
+        name: 'Pizza Spinaci',
+        price: 12,
+    },
+    {
+        name: 'Pizza Funghi',
+        price: 12,
+    },
+    {
+        name: 'Pizza Prosciutto',
+        price: 15,
+    },
+];
