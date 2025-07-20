5f7621d7ffa6dac3de045c034cbdae95a1711a44
Kiran Anand <liteninkiran@hotmail.com>
2025-07-20 21:31:31 +0100

    406 - Final Touches + Fixing Bugs

--- 17-the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ 17-the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -16,6 +16,7 @@ import { useCheckout } from '@features/check-in-out/useCheckout';
 import Modal from '@ui/Modal';
 import ConfirmDelete from '@ui/ConfirmDelete';
 import { useDeleteBooking } from './useDeleteBooking';
+import Empty from '@ui/Empty';
 
 const HeadingGroup = styled.div`
     display: flex;
@@ -30,6 +31,10 @@ const BookingDetail = () => {
     const moveBack = useMoveBack();
     const { checkout, isCheckingOut } = useCheckout();
     const { deleteBooking, isDeleting } = useDeleteBooking();
+
+    if (!booking) {
+        return <Empty resource='booking' />;
+    }
 
     if (isLoading) return <Spinner />;
 
