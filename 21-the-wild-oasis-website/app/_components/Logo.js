fcb2f59ae2c48ea7df8388469b145b316069d9fb
Kiran Anand <liteninkiran@hotmail.com>
2025-07-22 20:16:54 +0100

    438 - Optimizing Images With Next.js <Image /> Component

--- 21-the-wild-oasis-website/app/_components/Logo.js
+++ 21-the-wild-oasis-website/app/_components/Logo.js
@@ -1,13 +1,16 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import logo from '@/public/logo.png';
 
 const Logo = () => {
     return (
         <Link href='/' className='flex items-center gap-4 z-10'>
-            <img
+            <Image
                 height='60'
                 width='60'
+                quality={100}
                 alt='The Wild Oasis logo'
-                src='/logo.png'
+                src={logo}
             />
             <span className='text-xl font-semibold text-primary-100'>
                 The Wild Oasis
--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -1,8 +1,6 @@
-import Logo from '@/app/_components/Logo';
-import Navigation from '@/app/_components/Navigation';
+import Header from '@/app/_components/Header';
 import '@/app/_styles/globals.css';
 import { Josefin_Sans } from 'next/font/google';
-import Header from './_components/Header';
 
 export const metadata = {
     title: {
@@ -25,7 +23,7 @@ const RootLayout = ({ children }) => {
                 className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
             >
                 <Header />
-                <div className='flex-1 px-8 py-12 grid bg-red-800'>
+                <div className='flex-1 px-8 py-12 grid'>
                     <main className='max-w-7xl mx-auto w-full'>{children}</main>
                 </div>
             </body>
