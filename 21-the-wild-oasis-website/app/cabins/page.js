440185c5704eeec3b497eb6985d861ad99f08c92
Kiran Anand <liteninkiran@hotmail.com>
2025-07-22 12:56:43 +0100

    426 - Fetching Data in a Page

--- 21-the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/app/cabins/page.js
@@ -1,5 +1,17 @@
-const Page = () => {
-    return <h1>Cabins</h1>;
+const Page = async () => {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    const res = await fetch(url);
+    const data = await res.json();
+    return (
+        <div>
+            <h1>Cabins</h1>
+            <ul>
+                {data.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Page;
