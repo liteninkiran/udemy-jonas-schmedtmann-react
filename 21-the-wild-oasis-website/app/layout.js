92f66d6e9be4338198c1b05f4c62313c87a2510e
Kiran Anand <liteninkiran@hotmail.com>
2025-07-22 20:13:50 +0100

    437 - Improving the Navigation and Root Layout

--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -2,6 +2,7 @@ import Logo from '@/app/_components/Logo';
 import Navigation from '@/app/_components/Navigation';
 import '@/app/_styles/globals.css';
 import { Josefin_Sans } from 'next/font/google';
+import Header from './_components/Header';
 
 export const metadata = {
     title: {
@@ -21,14 +22,12 @@ const RootLayout = ({ children }) => {
     return (
         <html lang='en'>
             <body
-                className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+                className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
             >
-                <header>
-                    <Logo />
-                    <Navigation />
-                </header>
-                <main>{children}</main>
-                <footer>Copyright by The Wild Oasis</footer>
+                <Header />
+                <div className='flex-1 px-8 py-12 grid bg-red-800'>
+                    <main className='max-w-7xl mx-auto w-full'>{children}</main>
+                </div>
             </body>
         </html>
     );
