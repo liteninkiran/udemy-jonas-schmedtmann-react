960fd45a4ea7e71272f4fd29b1e2793b4faf4f24
Kiran Anand <liteninkiran@hotmail.com>
2025-07-22 20:07:09 +0100

    436 - Loading and Optimizing Fonts

--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -1,6 +1,7 @@
 import Logo from '@/app/_components/Logo';
 import Navigation from '@/app/_components/Navigation';
 import '@/app/_styles/globals.css';
+import { Josefin_Sans } from 'next/font/google';
 
 export const metadata = {
     title: {
@@ -11,10 +12,17 @@ export const metadata = {
         'Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and forests.',
 };
 
+const josefin = Josefin_Sans({
+    subsets: ['latin'],
+    display: 'swap',
+});
+
 const RootLayout = ({ children }) => {
     return (
         <html lang='en'>
-            <body className={`bg-primary-950 text-primary-100 min-h-screen`}>
+            <body
+                className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+            >
                 <header>
                     <Logo />
                     <Navigation />
