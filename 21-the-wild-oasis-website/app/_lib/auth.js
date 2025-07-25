7f6c63cdbe6ae4f6c949c35d212be4e4cb186a77
Kiran Anand <liteninkiran@hotmail.com>
2025-07-25 11:15:45 +0100

    472 - Getting the User Session

--- 21-the-wild-oasis-website/app/_lib/auth.js
+++ 21-the-wild-oasis-website/app/_lib/auth.js
@@ -7,12 +7,37 @@ const googleCreds = {
     clientSecret: process.env.AUTH_GOOGLE_SECRET,
 };
 const providers = [Google(googleCreds)];
+const authorized = ({ auth, request }) => !!auth?.user;
+const signIn1 = async ({ user, account, profile }) => {
+    try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+            await createGuest({
+                email: user.email,
+                fullName: user.name,
+            });
+
+        return true;
+    } catch {
+        return false;
+    }
+};
+const session = async () => {
+    const guest = await getGuest(session.user.email);
+    session.user.guestId = guest.id;
+    return session;
+};
 
 const authConfig = {
     providers,
+    // callbacks: { authorized, signIn: signIn1, session },
+    // pages: { signIn: '/login' },
 };
 
 export const {
     auth,
+    // signIn,
+    // signOut,
     handlers: { GET, POST },
 } = NextAuth(authConfig);
